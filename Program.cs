c32d3b76396bb9b61a796c7e2335834258f8f09e
MichaelGallinago <67271938+MichaelGallinago@users.noreply.github.com>
2022-10-08 15:51:40 +0500

    Fix

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] words = { "code", "doce", "ecod", "framer", "frame" };
+
+        List<string> uniqueAnagrams = GetUniqueAnagram(words);
+        uniqueAnagrams.Sort();
+        string[] result = uniqueAnagrams.ToArray();
+
+        foreach (var word in result) Console.Write(word + " ");
+        Console.WriteLine();
+    }
+
+    public static List<string> GetUniqueAnagram(string[] words)
+    {
+        List<string> uniqueAnagrams = new List<string>();
+
+        for (var i = 0; i < words.Length; i++)
+        {
+            char[] mainLeters = words[i].ToCharArray();
+            Array.Sort(mainLeters);
+
+            bool isAnagram = false;
+
+            for (var j = i - 1; j >= 0; j--)
+            {
+                char[] compareLeters = words[j].ToCharArray();
+                Array.Sort(compareLeters);
+                isAnagram = Enumerable.SequenceEqual(mainLeters, compareLeters);
+                if (isAnagram) break;
+            }
+            if (isAnagram) continue;
+            uniqueAnagrams.Add(words[i]);
+        }
+
+        return uniqueAnagrams;
+    }
+}
